0cbc3350b3d629809b28bb956094cd9c852aa153
Frederik Winther <frederikwinther@msn.com>
2017-11-30 17:24:46 +0100

    Changed resolution to 2x1920. Added extra player and ball.

---
+++ Assets/Scripts/TwoViews.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoViews : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Screen.SetResolution(3840, 1080, false);
+	}
+}
